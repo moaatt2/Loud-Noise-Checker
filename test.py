93cd89e513ed09aaa7f439f96a91d1d8b62bf183
Dakota McKay <dakotamckay72@gmail.com>
2025-08-26 19:13:27 -0400

    Use RMS to summarize data while emmphasizing loud sounds.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 
+from collections import deque
 import sounddevice as sd
 import numpy as np
 
@@ -7,7 +8,11 @@ duration = 0.5
 def audio_callback(indata, frames, time, status):
     if status:
         print(status)
-    print(indata)
+
+    # print(indata) # Raw audio data
+
+    # Use Root of Mean Square to create a single value for volume that emphasizes louder sounds
+    rms = np.sqrt(np.mean(indata**2))
 
 with sd.InputStream(callback=audio_callback, channels=1, samplerate=44100, blocksize=int(44100 * duration)):
     print("Recording... Press Ctrl+C to stop.")
