1d45d8a1f70a8327c784c1d7850f28e989dccdf5
Dakota McKay <dakotamckay72@gmail.com>
2025-08-27 18:30:46 -0400

    Use a double ended queue to record fixed amount of observations.

--- test.py
+++ test.py
@@ -3,7 +3,15 @@ from collections import deque
 import sounddevice as sd
 import numpy as np
 
+# How many seconds of audio in each sample
 duration = 0.5
+
+# Set how many samples to keep in memory
+num_samples = 25
+
+# Instantiate a double ended queue to hold samples
+samples = deque(maxlen=num_samples)
+
 
 def audio_callback(indata, frames, time, status):
     if status:
@@ -13,6 +21,12 @@ def audio_callback(indata, frames, time, status):
 
     # Use Root of Mean Square to create a single value for volume that emphasizes louder sounds
     rms = np.sqrt(np.mean(indata**2))
+
+    samples.append(rms)
+
+    print(len(samples), "samples recorded. Latest RMS:", rms)
+
+
 
 with sd.InputStream(callback=audio_callback, channels=1, samplerate=44100, blocksize=int(44100 * duration)):
     print("Recording... Press Ctrl+C to stop.")
